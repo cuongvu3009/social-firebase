947e4ac264f35487f32c3ca8f7fdde943c2901e7
cuongvu3009 <cuongvu3009@gmail.com>
2022-09-22 16:55:13 +0300

    hook and context setup

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import { projectAuth, projectFirestore } from '../firebase/config';
+import { useAuthContext } from './useAuthContext';
+
+export const useSignup = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const signup = async (name, email, password, thumbnail) => {
+    //	reset state
+    setError(null);
+    setIsPending(true);
+
+    try {
+      //	create user
+      const res = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      if (!res) {
+        throw new Error('Could not complete sign up');
+      }
+
+      // dispatch login action
+      dispatch({ type: 'LOGIN', payload: res.user });
+
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
+    } catch (error) {
+      if (!isCancelled) {
+        setError(error.message);
+        setIsPending(false);
+      }
+    }
+  };
+
+  //	clean up hook
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { signup, error, isPending };
+};
