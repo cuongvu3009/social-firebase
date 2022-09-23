7085f344ab725ab8e83e031f86e5705dc5aaccfb
cuongvu3009 <cuongvu3009@gmail.com>
2022-09-23 10:31:57 +0300

    useCollection, useFirestore, useLogout

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { projectAuth } from '../firebase/config';
+import { useAuthContext } from './useAuthContext';
+
+export const useLogout = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+  const { dispatch } = useAuthContext();
+
+  const logout = async () => {
+    try {
+      // sign the user out
+      await projectAuth.signOut();
+
+      // dispatch logout action
+      dispatch({ type: 'LOGOUT' });
+
+      // update state
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
+    } catch (error) {
+      if (isCancelled) {
+        setError(error.message);
+        setIsPending(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  });
+
+  return { logout, error, isPending };
+};
