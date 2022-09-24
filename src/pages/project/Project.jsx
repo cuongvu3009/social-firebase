f2ed4359ad1bc42879b77c39f93025928feb2721
cuongvu3009 <cuongvu3009@gmail.com>
2022-09-24 14:41:20 +0300

    fetch single project

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useDocument } from '../../hooks/useDocument';
 
-const Project = () => {
-  return <div>Project</div>;
-};
+// styles
+import './project.css';
 
-export default Project;
+export default function Project() {
+  const { id } = useParams();
+  const { document, error } = useDocument('projects', id);
+
+  if (error) {
+    return <div className='error'>{error}</div>;
+  }
+  if (!document) {
+    return <div className='loading'>Loading...</div>;
+  }
+
+  return (
+    <div className='project-details'>
+      <h1>{document.name}</h1>
+    </div>
+  );
+}
