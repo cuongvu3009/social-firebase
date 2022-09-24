007318bed9d2c0ac2260ca4b9925f5a1d7d31861
cuongvu3009 <cuongvu3009@gmail.com>
2022-09-24 14:33:24 +0300

    display projects

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { useCollection } from '../../hooks/useCollection';
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
+// components
+import ProjectList from '../../components/projectList/ProjectList';
 
-export default Dashboard;
+// styles
+import './dashboard.css';
+
+export default function Dashboard() {
+  const { documents, error } = useCollection('projects');
+
+  return (
+    <div>
+      <h2 className='page-title'>Dashboard</h2>
+      {error && <p className='error'>{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
+}
